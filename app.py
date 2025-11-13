4a1ad06a1ccc8799c655aeec84adae977dbd5bd4
quamrul hoda <162465089+quamrl-hoda@users.noreply.github.com>
2025-11-14 00:02:56 +0530

    Remove unused import from app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # -- end of fix --
 import streamlit as st
 from streamlit import spinner
-from streamlit.web.server.server import server_port_is_manually_set
 
 from supporting_functions import (
      extract_video_id,
